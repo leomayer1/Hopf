032a45a9031a30f37bc3bc803b1336d95203a809
leopoldmayer <leomayer@uw.edu>
2022-04-07 15:37:19 -0700

    Restructured hopf_algebra class to use mathlib's R-algebras.

--- src/Hopf.lean
+++ src/Hopf.lean
@@ -1,43 +1,33 @@
-import linear_algebra.tensor_product
+import ring_theory.tensor_product
 import algebra.monoid_algebra.basic
+import data.polynomial.algebra_map
 
 open_locale tensor_product
 
-section hopf_alg
-
-open tensor_product
-open linear_map
+open algebra.tensor_product
 
 variables (K : Type) [field K]
-variables (V : Type) [add_comm_group V] [module K V]
+variables (V : Type) [semiring V] [algebra K V]
 
 
-namespace Hopf
+
 /- A coalgebra class, i.e a vector space with maps which look like the algebra maps,
   but with arrows flipped.
   Maybe it would be nicer to make classes for has_comul, has_counit, has_coassoc, etc.
 -/
-class coalgebra :=
-(comul : V →ₗ[K] V ⊗[K] V)
-(counit : V →ₗ[K] K)
-(coassoc : (tensor_product.assoc K V V V) ∘ (map comul id) ∘ comul = map id comul ∘ comul)
-(counit_left : (tensor_product.lid K V) ∘ (map counit id) ∘ comul = id)
-(counit_right : (tensor_product.rid K V) ∘ (map id counit) ∘ comul = id)
 
-class algebra :=
-(mul : V ⊗[K] V →ₗ[K] V)
-(unit : K →ₗ[K] V)
-(assoc : mul ∘ (map mul id) = mul ∘ (map id mul) ∘ tensor_product.assoc K V V V)
-(unit_left : mul ∘ (map unit id) = (tensor_product.lid K V))
-(unit_right : mul ∘ (map id unit) = (tensor_product.rid K V))
-
-class bialgebra extends algebra K V, coalgebra K V
-
-class hopf_algebra extends bialgebra K V :=
+class hopf_algebra extends algebra K V :=
+(comul : V →ₐ[K] V ⊗ V)
+(counit : V →ₐ[K] K)
+(coassoc : (tensor_product.assoc K V V V) ∘ (map comul (alg_hom.id K V)) ∘ comul = map (alg_hom.id K V) comul ∘ comul)
+(counit_left : (tensor_product.lid K V) ∘ (map counit (alg_hom.id K V)) ∘ comul = (alg_hom.id K V))
+(counit_right : (tensor_product.rid K V) ∘ (map (alg_hom.id K V) counit) ∘ comul = (alg_hom.id K V))
 (coinv : V →ₗ[K] V)
-(coinv_right : mul ∘ (map id coinv) ∘ comul = unit ∘ counit)
-(coinv_left : mul ∘ (map coinv id) ∘ comul = unit ∘ counit)
+--(coinv_right : mul ∘ (map id coinv) ∘ comul = unit ∘ counit)
+--(coinv_left : mul ∘ (map coinv id) ∘ comul = unit ∘ counit)
 
 /- Group ring of an abelian group is actually a hopf algebra? -/
-end Hopf
-end hopf_alg
+
+notation K`[x]`:9000 := polynomial K
+
+--def comul : K[x] →ₗ[K] K[x] ⊗ K[x] := sorry
