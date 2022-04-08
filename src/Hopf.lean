0d1607a724573424c24c9bccb8836caf2aa6c642
leopoldmayer <leomayer@uw.edu>
2022-04-07 17:06:43 -0700

    Fixed hopf_alg structure, no errors in definition

--- src/Hopf.lean
+++ src/Hopf.lean
@@ -1,33 +1,23 @@
 import ring_theory.tensor_product
-import algebra.monoid_algebra.basic
-import data.polynomial.algebra_map
 
 open_locale tensor_product
-
 open algebra.tensor_product
 
-variables (K : Type) [field K]
-variables (V : Type) [semiring V] [algebra K V]
-
-
-
-/- A coalgebra class, i.e a vector space with maps which look like the algebra maps,
-  but with arrows flipped.
-  Maybe it would be nicer to make classes for has_comul, has_counit, has_coassoc, etc.
+/- A big structure. There's a lot of uglinesshere...
+  So much of the text here is making natural isomorphisms explicit...
+  `tensor_product.assoc K V V V` is associativiy of tensor product,
+  `map` is taking the tensor product of two algebra maps
+  `alg_hom.id` is the identity, as an algebra map
+  `tensor_product.lid` is saying that K is a left identity for K algebras under tensoring
+  `algebra.of_id` is the structure morphism K →ₐ[K] V
+  Should I add notation for these? Is there a way to have lean automatically do these?
 -/
-
-class hopf_algebra extends algebra K V :=
-(comul : V →ₐ[K] V ⊗ V)
+structure hopf_algebra (K : Type) [field K] (V : Type) [comm_ring V] [algebra K V] :=
+(comul : V →ₐ[K] (V ⊗[K] V))
 (counit : V →ₐ[K] K)
 (coassoc : (tensor_product.assoc K V V V) ∘ (map comul (alg_hom.id K V)) ∘ comul = map (alg_hom.id K V) comul ∘ comul)
 (counit_left : (tensor_product.lid K V) ∘ (map counit (alg_hom.id K V)) ∘ comul = (alg_hom.id K V))
 (counit_right : (tensor_product.rid K V) ∘ (map (alg_hom.id K V) counit) ∘ comul = (alg_hom.id K V))
-(coinv : V →ₗ[K] V)
---(coinv_right : mul ∘ (map id coinv) ∘ comul = unit ∘ counit)
---(coinv_left : mul ∘ (map coinv id) ∘ comul = unit ∘ counit)
-
-/- Group ring of an abelian group is actually a hopf algebra? -/
-
-notation K`[x]`:9000 := polynomial K
-
---def comul : K[x] →ₗ[K] K[x] ⊗ K[x] := sorry
+(coinv : V →ₐ[K] V)
+(coinv_right : (lmul' K) ∘ (map (alg_hom.id K V) coinv) ∘ comul = (algebra.of_id K V) ∘ counit)
+(coinv_left : (lmul' K) ∘ (map coinv (alg_hom.id K V)) ∘ comul = (algebra.of_id K V) ∘ counit)
