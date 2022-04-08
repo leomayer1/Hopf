b2fc5d960731afa4113c510939005b3f14757935
Vasily Ilin <vasilin97@gmail.com>
2022-04-07 17:10:24 -0700

    wrote comul and counit

--- src/Hopf.lean
+++ src/Hopf.lean
@@ -9,15 +9,8 @@ open algebra.tensor_product
 variables (K : Type) [field K]
 variables (V : Type) [semiring V] [algebra K V]
 
-
-
-/- A coalgebra class, i.e a vector space with maps which look like the algebra maps,
-  but with arrows flipped.
-  Maybe it would be nicer to make classes for has_comul, has_counit, has_coassoc, etc.
--/
-
-class hopf_algebra extends algebra K V :=
-(comul : V →ₐ[K] V ⊗ V)
+structure hopf_algebra :=
+(comul : V →ₐ[K] V ⊗[K] V)
 (counit : V →ₐ[K] K)
 (coassoc : (tensor_product.assoc K V V V) ∘ (map comul (alg_hom.id K V)) ∘ comul = map (alg_hom.id K V) comul ∘ comul)
 (counit_left : (tensor_product.lid K V) ∘ (map counit (alg_hom.id K V)) ∘ comul = (alg_hom.id K V))
@@ -26,8 +19,16 @@ class hopf_algebra extends algebra K V :=
 --(coinv_right : mul ∘ (map id coinv) ∘ comul = unit ∘ counit)
 --(coinv_left : mul ∘ (map coinv id) ∘ comul = unit ∘ counit)
 
-/- Group ring of an abelian group is actually a hopf algebra? -/
+notation K`[X]`:9000 := polynomial K
 
-notation K`[x]`:9000 := polynomial K
+noncomputable def comul : K[X] →ₐ[K] K[X] ⊗[K] K[X] := polynomial.aeval ((polynomial.X ⊗ₜ 1) + (1 ⊗ₜ polynomial.X))
 
---def comul : K[x] →ₗ[K] K[x] ⊗ K[x] := sorry
+notation `Δ` :9000 := comul
+
+noncomputable def counit : K[X] →ₐ[K] K := polynomial.aeval 0
+
+-- noncomputable lemma coassoc : (tensor_product.assoc K K[X] K[X] K[X]) ∘ (map (Δ K) (alg_hom.id K K[X])) ∘ (Δ K) = (map (alg_hom.id K K[X]) (Δ K)) ∘ (Δ K)) := sorry
+-- begin
+--   sorry
+-- end
+
