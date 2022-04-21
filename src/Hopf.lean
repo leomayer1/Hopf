1b391b977885b1046feb087b48a420ab22322979
Vasily Ilin <vasilin97@gmail.com>
2022-04-21 13:18:12 -0700

    change hopf_algebra to class

--- src/Hopf.lean
+++ src/Hopf.lean
@@ -3,8 +3,8 @@ import ring_theory.tensor_product
 open_locale tensor_product
 open algebra.tensor_product
 
-variables (K : Type) [field K]
-variables (V : Type) [comm_ring V] [algebra K V]
+variables (K : Type*) [comm_ring K]
+variables (V : Type*) [comm_ring V] [algebra K V]
 
 /- A big structure. There's a lot of uglinesshere...
   So much of the text here is making natural isomorphisms explicit...
@@ -15,13 +15,14 @@ variables (V : Type) [comm_ring V] [algebra K V]
   `algebra.of_id` is the structure morphism K →ₐ[K] V
   Should I add notation for these? Is there a way to have lean automatically do these?
 -/
-structure hopf_algebra :=
+class hopf_algebra :=
 (comul : V →ₐ[K] V ⊗[K] V)
 (counit : V →ₐ[K] K)
-(coassoc : (tensor_product.assoc K V V V) ∘ (map comul (alg_hom.id K V)) ∘ comul = map (alg_hom.id K V) comul ∘ comul) -- TODO make this an equality of algebra morphisms instead of just k-linear maps. See https://github.com/leanprover-community/mathlib/blob/9498beac24f2924fe0e9994554d2226f4472ef87/src/ring_theory/tensor_product.lean#L673 for associativity of tensor product of algebras
+(coassoc : (tensor_product.assoc K V V V) ∘ (map comul (alg_hom.id K V)) ∘ comul = map (alg_hom.id K V) comul ∘ comul)
 (counit_left : (tensor_product.lid K V) ∘ (map counit (alg_hom.id K V)) ∘ comul = (alg_hom.id K V))
 (counit_right : (tensor_product.rid K V) ∘ (map (alg_hom.id K V) counit) ∘ comul = (alg_hom.id K V))
 (coinv : V →ₐ[K] V)
 (coinv_right : (lmul' K) ∘ (map (alg_hom.id K V) coinv) ∘ comul = (algebra.of_id K V) ∘ counit)
 (coinv_left : (lmul' K) ∘ (map coinv (alg_hom.id K V)) ∘ comul = (algebra.of_id K V) ∘ counit)
-
+.
+#check hopf_algebra
--- src/polynomial_example.lean
+++ src/polynomial_example.lean
@@ -1,3 +1,4 @@
+import Hopf
 import ring_theory.tensor_product
 import tactic
 
@@ -6,12 +7,11 @@ open_locale polynomial
 open algebra.tensor_product
 open polynomial
 
-universe u
-variables (K : Type u) [comm_ring K]
+variables (K : Type*) [comm_ring K]
 
 -- set_option profiler true -- time everything
 
-@[simp] lemma assoc_symm_tmul {R : Type u} [comm_semiring R] {A : Type u} [semiring A] [algebra R A] {B : Type u} [semiring B] [algebra R B] {C : Type u} [semiring C] [algebra R C] (a : A) (b : B) (c : C) :
+@[simp] lemma assoc_symm_tmul {R : Type*} [comm_semiring R] {A : Type*} [semiring A] [algebra R A] {B : Type*} [semiring B] [algebra R B] {C : Type*} [semiring C] [algebra R C] (a : A) (b : B) (c : C) :
 (algebra.tensor_product.assoc R A B C).symm (a ⊗ₜ[R] (b ⊗ₜ[R] c)) = a ⊗ₜ[R] b ⊗ₜ[R] c :=
 begin
   apply_fun (algebra.tensor_product.assoc R A B C),
@@ -21,13 +21,13 @@ end
 
 noncomputable def comul : K[X] →ₐ[K] K[X] ⊗[K] K[X] := aeval ((X ⊗ₜ 1) + (1 ⊗ₜ X))
 
-@[simp] lemma comul_1 : comul K (1 : K[X]) = (1 ⊗ₜ 1 : K[X] ⊗[K] K[X]) := -- elaboration of comul_1 took 4.18s
+@[simp] lemma comul_1 : comul K (1 : K[X]) = (1 ⊗ₜ 1 : K[X] ⊗[K] K[X]) :=
 begin 
   rw alg_hom.map_one, -- ⊢ 1 = 1 ⊗ₜ[K] 1
   refl,
 end
 
-@[simp] lemma comul_X : comul K X = X ⊗ₜ 1 + 1 ⊗ₜ X := -- elaboration of comul_X took 257ms
+@[simp] lemma comul_X : comul K X = X ⊗ₜ 1 + 1 ⊗ₜ X :=
 begin 
   unfold comul,
   rw aeval_X,
@@ -46,20 +46,16 @@ begin
   simp only [map1, map2, alg_equiv.to_alg_hom_eq_coe, alg_hom.coe_comp, alg_equiv.coe_alg_hom,
     function.comp_app, comul_X, map_tmul, alg_hom.coe_id, id.def, comul_1,
     tensor_product.tmul_add, add_assoc, assoc_symm_tmul, tensor_product.add_tmul, _root_.map_add],
-  -- have P1 : map1 K X = X ⊗ₜ 1 ⊗ₜ 1 + 1 ⊗ₜ X ⊗ₜ 1 + 1 ⊗ₜ 1 ⊗ₜ X,
-  --   unfold map1,
-  --   simp [tensor_product.tmul_add, add_assoc],
-
-  -- have P2 : map2 K X = X ⊗ₜ 1 ⊗ₜ 1 + 1 ⊗ₜ X ⊗ₜ 1 + 1 ⊗ₜ 1 ⊗ₜ X,
-  --   unfold map2,
-  --   simp only [tensor_product.add_tmul, alg_hom.coe_comp, function.comp_app, comul_X, _root_.map_add, map_tmul, alg_hom.coe_id, id.def, comul_1],
-  
-  -- ext,
-  -- rw P1,
-  -- rw P2,
 end
 
-noncomputable def counit : K[X] →ₐ[K] K := polynomial.aeval 0
+noncomputable def counit : K[X] →ₐ[K] K := aeval 0
+
+@[simp] lemma counit_X : counit K X = 0 :=
+begin
+  unfold counit,
+  simp only [coe_aeval_eq_eval, eval_X],
+end
+
 .
 
 lemma counit_left : (algebra.tensor_product.lid K K[X]).to_alg_hom.comp ((map (counit K) (alg_hom.id K K[X])).comp (comul K)) = (alg_hom.id K K[X]) :=
@@ -76,8 +72,26 @@ end
 
 noncomputable def coinv : K[X] →ₐ[K] K[X] := aeval (-X)
 
--- TODO
--- lemma coinv_right : (lmul' K).comp ((map (alg_hom.id K K[X]) (coinv K)).comp (comul K)) = (algebra.of_id K K[X]).comp (counit K) :=
--- begin
---   sorry
--- end
+@[simp] lemma coinv_X : coinv K X = -X :=
+begin
+  unfold coinv,
+  rw aeval_X,
+end
+
+lemma coinv_right : (lmul' K).comp ((map (alg_hom.id K K[X]) (coinv K)).comp (comul K)) = (algebra.of_id K K[X]).comp (counit K) :=
+begin
+  ext,
+  simp only [alg_hom.coe_comp, function.comp_app, comul_X, _root_.map_add, map_tmul, alg_hom.coe_id, id.def, _root_.map_one, coinv_X, lmul'_apply_tmul, _root_.mul_one, _root_.one_mul, add_right_neg, counit_X, _root_.map_zero],
+end
+
+lemma coinv_left : (lmul' K).comp ((map (coinv K) (alg_hom.id K K[X])).comp (comul K) )= (algebra.of_id K K[X]).comp (counit K) :=
+begin
+  ext,
+  simp only [alg_hom.coe_comp, function.comp_app, comul_X, _root_.map_add, map_tmul, coinv_X, alg_hom.coe_id, id.def, _root_.map_one, lmul'_apply_tmul, _root_.mul_one, _root_.one_mul, add_left_neg, counit_X, _root_.map_zero],
+end
+.
+
+instance polynomial_hopf : hopf_algebra K K[X] := 
+begin 
+  sorry,
+end 
